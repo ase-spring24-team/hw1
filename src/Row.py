56b77acbe4bc7afd40867f85f6c9a409f74575ca
ExtremeMachine12 <kwiatkowskislmartin@gmail.com>
2024-01-13 14:38:02 -0500

    Added Row class

---
+++ src/Row.py
@@ -0,0 +1,13 @@
+"""
+File by Samuel Kwiatkowski-Martin
+This file is our row class
+"""
+
+
+class Row:
+    """
+    Initializes the row using a passed array
+    param t: the array the represents the row we care about
+    """
+    def __init__(self, t):
+        self.cells = t
