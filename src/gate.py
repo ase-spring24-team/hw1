d4c116ca10f06e6286e0dc466cdaa17e35f8bf15
sairajzero <sairaj0630@gmail.com>
2024-01-14 18:56:32 -0500

    Create gate.py
    Co-Authored-By: sathiya06 <64639742+sathiya06@users.noreply.github.com>

---
+++ src/gate.py
@@ -0,0 +1,22 @@
+"""
+HW: week 2
+
+OPTIONS:
+    -c --cohen  small effect size               = .35
+    -f --file   csv data file name              = ../data/auto93.csv
+    -h --help   show help                       = False
+    -k --k      low class frequency kludge      = 1
+    -m --m      low attribute frequency kludge  = 2
+    -s --seed   random number seed              = 31210
+    -t --test   start up action                 = help
+"""
+
+import utility as l
+
+
+def run(x):
+    print(x)
+
+
+the = l.settings(__doc__)
+run(l.cli(the).test)
