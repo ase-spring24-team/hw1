391e49e5c962338c35e6499473cacf3478714c1e
ExtremeMachine12 <kwiatkowskislmartin@gmail.com>
2024-01-15 18:39:45 -0500

    Added egs test

--- src/Eg.py
+++ src/Eg.py
@@ -17,6 +17,14 @@ def num():
     print(rnd(mu, 3), rnd(sd, 3), mu, sd)
     return 9.9 < mu and mu < 10.2 and 3 < sd and sd < 5
 
+def egs():
+    """
+    Prints out all the different tests we can run
+    """
+    test_names = ["all", "egs", "help", "sym", "num", "csv", "data", "stats"]
+    for test_name in test_names:
+        print("lua gat.lua -t " + test_name)
 
 stats()
 print(num())
+egs()
