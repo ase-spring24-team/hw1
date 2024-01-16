2c9801c5a8fa96dc8d1a4996902440b3371a22ce
sairajzero <sairaj0630@gmail.com>
2024-01-15 19:36:06 -0500

    Adding comment to files Num.py and Sym.py

--- src/Num.py
+++ src/Num.py
@@ -1,5 +1,15 @@
+"""
+File by Sai Raj Thirumalai
+This file is our Num class, which will contain the numeric data
+"""
+
 class Num:
     def __init__(self, s=" ", n=0):
+        """
+        Initialization function for the Num class
+        Sets up the column name, column index, number of rows and data aggregates
+        :param s: column name, n: column index
+        """
         self.txt = s
         self.at = n
         self.n = 0
@@ -10,24 +20,43 @@ class Num:
         heaven = 0 if s.endswith("-") else 1
 
     def add(self, x):
-        if x != "?":
+        """
+        Function that adds a value
+        :param x: data value
+        """
+        if x != "?": # ignore if value is ?
             self.n += 1
+            # calculate mean value
             d = x - self.mu
             self.mu += d / self.n
             self.m2 += d * (x - self.mu)
+            # update high and low values
             self.lo = min(x, self.lo)
             self.hi = max(x, self.hi)
 
     def mid(self):
+        """
+        Function that returns the mid (mean) value
+        :return: mean
+        """
         return self.mu
 
     def div(self):
+        """
+        Function that returns the standard deviation
+        :return: standard deviation
+        """
         if self.n < 2:
             return 0
         else:
             return self.m2 / (self.n - 1)
 
     def norm(self, x):
+        """
+        Function that returns the normalized value
+        :param x: data value
+        :return: normalized value
+        """
         if x == "?":
             return x
         else:
--- src/Sym.py
+++ src/Sym.py
@@ -1,7 +1,17 @@
+"""
+File by Sai Raj Thirumalai
+This file is our Sym class, which will contain the Sym data
+"""
+
 import math
 
 class Sym:
     def __init__(self, s=" ", n=0):
+        """
+        Initialization function for the Sym class
+        Sets up the column name, column index, number of rows and data aggregates
+        :param s: column name, n: column index
+        """
         self.txt = s
         self.at = n
         self.n = 0
@@ -10,20 +20,34 @@ class Sym:
         self.most = 0
 
     def add(self, x):
-        if x != "?":
+        """
+        Function that adds a value
+        :param x: data value
+        """
+        if x != "?": # ignore if value is ?
             self.n += 1
+            # increase the count of value
             if x in self.has:
                 self.has[x] += 1
             else:
                 self.has[x] = 1
+            # update mode value if needed
             if self.has[x] > self.most:
                 self.most = self.has[x]
                 self.mode = x
 
     def mid(self):
+        """
+        Function that returns the mid (mode) value
+        :return: mean
+        """
         return self.mode
 
     def div(self):
+        """
+        Function that returns the standard deviation
+        :return: standard deviation
+        """
         e = 0
         for _, v in self.has.items():
             e -= v / self.n * math.log(v / self.n, 2)
