e9c622ac67ee9296293436ffa5f934fe4ce9c5b6
sairajzero <sairaj0630@gmail.com>
2024-01-15 21:10:29 -0500

    Update Eg.py

--- src/Eg.py
+++ src/Eg.py
@@ -8,14 +8,35 @@ from Num import Num
 from Sym import Sym
 from util import norm, rnd
 import util as l
-import gate
 
+tests = {}
+
+def all():
+    bad = 0
+    good = 0
+    for t_name in tests:
+        if t_name != "all":
+            print("--testing:", t_name)
+            if tests[t_name](): # test passed                
+                good += 1
+                print("Passed")
+            else: # test failed
+                bad += 1
+                print("Failed")
+    print("----")
+    print("TOTAL:", bad+good)
+    print("PASS:", good)
+    print("FAIL:", bad)
+    exit(bad)
 
 def stats():
     """
     Tests and prints out default for the stats function on auto93.csv
     """
-    print(Data("../data/auto93.csv").stats())
+    stat = Data("../data/auto93.csv").stats()
+    o_stat = l.o(stat)
+    print(o_stat)
+    return o_stat == "dict{.N:397 Acc+:15.57 Lbs-:2970.42 Mpg+:23.84}"
 
 
 def num():
@@ -40,9 +61,11 @@ def egs():
     """
     Prints out all the different tests we can run
     """
-    test_names = ["all", "egs", "help", "sym", "num", "csv", "data", "stats", "oo"]
+    #test_names = ["all", "egs", "help", "sym", "num", "csv", "data", "stats", "oo"]
+    test_names = tests.keys()
     for test_name in test_names:
         print("python gate.py -t " + test_name)
+    return True
 
 def data():
     n = 0
@@ -63,7 +86,8 @@ def csv():
     return n == 32
 
 def help():
-    print(gate.the.help)
+    print(the.help)
+    return True
 
 def oo():
     d = {"a":1,"b":2,"c":3,"d":4}
@@ -72,7 +96,7 @@ def oo():
     return l.o(d) == "dict{a:1 b:2 c:3 d:4}" 
 
 
-the = l.SLOTS({"file":"../data/auto93.csv"})
+the = l.SLOTS({"file":"../data/auto93.csv", "help": "somehelp"})
 
 
 def row():
@@ -80,20 +104,18 @@ def row():
     Tests the row class
     """
     print("[1, 2, 3] == " + str(Row([1,2,3]).cells))
+    return "[1, 2, 3] == " + str(Row([1,2,3]).cells)
 
-def all():
-    """
-    Tests all test functions
-    """
-    stats()
-    print(num())
-    egs()
-    the = l.SLOTS({"file": "../data/auto93.csv"})
-    print(data())
-    oo()
-    row()
-    sym()
-    csv()
-    help()
+# function to automatically load all functions in this module in test variable
+for (k, v) in list(locals().items()):
+    if callable(v) and v.__module__ == __name__:
+        tests[k] = v
+
+stats()
+print(num())
+egs()
+the = l.SLOTS({"file":"../data/auto93.csv"})
+print(data())
+oo()
 
 all()
