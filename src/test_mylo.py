8111b2dd2b8eaf349d613b2106ae0065864a2c40
ExtremeMachine12 <kwiatkowskislmartin@gmail.com>
2024-02-04 17:03:46 -0500

    Tests for Num
    Wrote 3 unit tests for num.dist

--- src/test_mylo.py
+++ src/test_mylo.py
@@ -4,6 +4,8 @@ Examples class -- essentially just the testing mylo related functions
 """
 from the import THE, the, SLOTS
 from Data import Data
+from Num import Num
+from Sym import Sym
 import util as l
 
 tests = {}
@@ -74,7 +76,38 @@ def dist():
         if i%30 == 0:
             print(i, rows[i].cells, round(rows[i].dist(r1, d),2))
 
+def test_num_dist_both_unknown():
+    """
+    Tests that the num.dist function is working as expected when 2 "?" values are input
+    """
+    n = Num()
+    assert n.dist("?","?") == 1
+
+def test_num_dist_one_unknown():
+    """
+    Tests that num.dist works as expected when only one value is unknown
+    """
+    n = Num()
+    for i in range(1, 11):
+        n.add(i)  # adding nums from 1 to 10
+    assert n.dist("?", 5) == 1-(4/9+1E-30)  # should be the exact output in this case
+    assert n.dist(5, "?") == 1-(4/9+1E-30)  #
+    assert n.dist("?", 8) == 7/9+1E-30
+    assert n.dist(8, "?") == 7/9+1E-30
+
+def test_num_dist_both_known():
+    """
+    Tests that num dist works when both x and y are known
+    """
+    n = Num()
+    for i in range(1, 11):
+        n.add(i)  # adding nums from 1 to 10
+    assert n.dist(6, 5) == (5 / 9 + 1E-30) - (4 / 9 + 1E-30)  # should be the exact output
+    assert n.dist(5, 6) == abs((4 / 9 + 1E-30)-(5 / 9 + 1E-30))
+
 if __name__ == '__main__':
     #all()
     the._set(SLOTS({"file":"../data/auto93.csv", "__help": "", "m":2, "k":1, "p":2}))
-    dist()
+    test_num_dist_one_unknown()
+    test_num_dist_both_unknown()
+    test_num_dist_both_known()
