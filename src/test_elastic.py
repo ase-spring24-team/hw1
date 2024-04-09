6be026b44a8e5e4891903e251646bb59170e6208
ExtremeMachine12 <kwiatkowskislmartin@gmail.com>
2024-04-09 01:25:59 -0400

    Update test_elastic.py
    Added csv writing and added error rates so now the elastic function creates a csv which can be ran on our smo classes.

--- src/test_elastic.py
+++ src/test_elastic.py
@@ -3,6 +3,7 @@ File created by Samuel Kwiatkowski-Martin
 This class represents
 """
 import csv
+import re
 import numpy as np
 from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import LinearRegression
@@ -41,13 +42,59 @@ def create_elastic_data_set(data_file):
     X = np.array(X)
     Y = np.array(Y)
 
+    # For creating and writing the csv file with a good naming convention
+    reg = r'[^./]*(?=\.[^./]*$)'
+    data_name = re.search(reg, data_file).group(0)
+    file_path = f'../data/{data_name}_elasticnet_hyperparameters.csv'
+
+    # Writing to CSV
+    with open(file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['alpha', 'l1_ratio', 'fit_intercept', 'max_iter', 'selection', 'warm_start', 'tol', 'error-'])
+        alpha = .1
+        while alpha <= 10:
+            l1_ratio = 0
+            while l1_ratio <= 1:
+                for fit_intercept in [True, False]:
+                    for max_iter in range(300, 5000, 500):
+                        for selection in ['cyclic', 'random']:
+                            for warm_start in [True, False]:
+                                tol = .00000001
+                                while tol <= .1:
+                                    regr = ElasticNet(random_state=0, alpha=alpha,
+                                                      l1_ratio=l1_ratio,
+                                                      fit_intercept=fit_intercept,
+                                                      max_iter=max_iter, selection=selection,
+                                                      warm_start=warm_start, tol=tol)
+                                    regr.fit(X, Y)
+                                    prediction_data_point = np.array(
+                                        [data.rows[-1].cells[:amount_of_x_values]])
+                                    prediction2 = regr.predict(prediction_data_point)
+                                    actual = np.array(data.rows[-1].cells[-amount_of_y_values:])
+                                    error = actual - prediction2
+                                    error = abs(error / actual)
+                                    error = np.linalg.norm(error - np.array([0, 0, 0]))
+                                    print(f"Elastic Net {prediction2}. Error {error}")
+                                    writer.writerow([alpha, l1_ratio, fit_intercept, max_iter, selection, warm_start, tol, error])
+                                    tol *= 10
+
+                l1_ratio += .1
+            alpha += .1
+    """
     regr = ElasticNet(random_state=0)
     regr.fit(X, Y)
     prediction_data_point = np.array([data.rows[-1].cells[:amount_of_x_values]])
     prediction2 = regr.predict(prediction_data_point)
-
+    print(prediction2[0])
+    actual = np.array(data.rows[-1].cells[-amount_of_y_values:])
+    error = actual - prediction2
+    error = abs(error/actual)
+    print(error)
+    error = np.linalg.norm(error-np.array([0,0,0]))
     print(f"Elastic Net {prediction2}")
-    print(f"Real Answer {data.rows[-1].cells[-amount_of_y_values:]}")
+    print(f"Real Answer {actual}")
+    print(f"Error: {error}")
+    """
 
 if __name__ == '__main__':
     the._set(SLOTS({"file":"../data/auto93.csv", "__help": "", "m":2, "k":1, "p":2, "Half":256, "d":32, "D":4,
